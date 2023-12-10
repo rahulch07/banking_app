b1fa8437ecc9fd27f010735a51a34f1c5e7e0cd3
rahulch07 <87270395+rahulch07@users.noreply.github.com>
2023-12-10 20:44:39 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://banking-app-api-mu.vercel.app',
 });
 
 // Add a request interceptor to include the access token in the headers
